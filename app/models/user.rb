67ed812b0c0f35ab94f13a8afa7136a83d1c76c0
Raymond <raymond@philippinedev.com>
2021-09-01 11:42:59 +0800

    refactor ui

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs
+
+  def name
+    email.split('@')[0].capitalize
+  end
+
 end
