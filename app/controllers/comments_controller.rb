c212544a30e21eb340a056099cebb34b2c63a580
Raymond <raymond@philippinedev.com>
2021-09-01 20:46:02 +0800

    comment posting enabled

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def create
+    @blog = Blog.find(params[:blog_id])
+    @blog.comments.create(comment_params)
+    if @blog.comments.create
+      redirect_to blog_path(@blog)
+    end
+    # @comment = current_user.comments.new(comment_params)
+    # if @comment.save
+    #   redirect_to blog_path
+    # end
+  end
+
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:content, :blog_id, :user_id)
+      # .merge(blog_id: params[:blog_id])
+  end
+
 end
