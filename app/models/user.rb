687b922104c0ad7e6d2f1f5dc4073936bbcfd315
Raymond <raymond@philippinedev.com>
2021-09-02 18:00:27 +0800

    add devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :blogs
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   def name
     email.split('@')[0].capitalize
